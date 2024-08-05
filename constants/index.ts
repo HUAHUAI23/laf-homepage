a6f323079074aa710536bb566991b5dac8f19173
HUAHUAI23 <huahua1319873800@outlook.com>
2024-08-05 07:51:34 +0000

    ok

--- constants/index.ts
+++ constants/index.ts
@@ -10,7 +10,7 @@ export const Routes = {
 export const site_url = {
   laf_github: "https://github.com/labring/laf",
   laf_index_video: "https://itceb8-video.oss.laf.run/laf-website.mp4",
-  sealaf: "https://gzg.sealos.run?openapp=system-sealaf",
+  sealaf: "https://bja.sealos.run?openapp=system-sealaf",
 }
 
 export const CHAKRA_UI_COLOR_MODE_KEY = "chakra-ui-color-mode"
